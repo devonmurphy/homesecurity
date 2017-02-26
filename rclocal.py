629ffc57ba8795b08da0e2e5d8688df42765ce1a
Devon Murphy <devonmurphy1@gmail.com>
2017-02-26 22:17:28 +0000

    added more comments

--- rclocal.py
+++ rclocal.py
@@ -9,9 +9,11 @@ dirPath = os.path.dirname(os.path.realpath(__file__))
 command = 'python '+dirPath+'/start-server.py &';
 fileName = '/etc/rc.local'
 
+#open the file and read the all the lines into the list content
 with open(fileName,"rw") as f:
     content = f.readlines()
 
+#This adds the command which starts the server to the list content
 lineCount = 0
 for line in content:
     if line == command+"\n":
@@ -19,11 +21,13 @@ for line in content:
         f.close()
         exit()
     if line == "exit 0\n":
+        #add the command to the content list and then exit out
         content.insert(lineCount,command+"\n")
         f.close()
         break
     lineCount+=1
 
-file2 = open(fileName,"w")
+#open the file again so that it can be replace with the list content
+f = open(fileName,"w")
 for line in content:
-    file2.write(line)
+    f.write(line)
